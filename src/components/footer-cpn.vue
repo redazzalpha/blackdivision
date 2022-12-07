d6f82531ed545e50d31295261839739d61d22a96
kyzer <redazzkillah@gmail.com>
2022-12-07 04:11:12 +0100

    add follow links on footer

--- src/components/footer-cpn.vue
+++ src/components/footer-cpn.vue
@@ -1,30 +1,84 @@
 <template>
-  <v-footer tag="footer" class="py-10" padless color="transparent">
+  <v-footer tag="footer" class="py-10 text-center" padless color="transparent">
+    <v-row>
+      <v-col class="col-12 col-sm-6">
+        <v-container fluid>
+          <v-row>
+            <v-col>
+              <v-row tag="nav" justify="center" no-gutters>
+                <v-btn
+                  v-for="link in links"
+                  :key="link.label"
+                  :to="link.href"
+                  color="white"
+                  text
+                  rounded
+                  class="link my-2"
+                  style="
+                    font-family: 'Raleway', sans-serif;
+                    font-weight: bold;
+                    text-decoration: unset;
+                  "
+                >
+                  {{ link.label }}
+                </v-btn>
+              </v-row>
 
-    <v-row tag='nav' justify="center" no-gutters>
-      <v-btn
-        v-for="link in links"
-        :key="link.label"
-        :to="link.href"
-        color="white"
-        text
-        rounded
-        class="link my-2"
-        style="font-family: 'Raleway', sans-serif; font-weight: bold; text-decoration: unset;"
-      >
-        {{ link.label }}
-      </v-btn>
-    </v-row>
+              <v-row justify="center" no-gutter>
+                <v-col class="white--text"> Retrouvez-nous sur: </v-col>
+              </v-row>
 
-    <v-row justify="center" no-gutters>
-      <v-col
-        class="py-4 text-center white--text"
-        cols="12"
-        style="font-style: italic"
-      >
-        &copy; Copyright {{ new Date().getFullYear() }} <br />
-        All rights reserved. <br v-show="$vuetify.breakpoint.name == 'xs'"/> 
-        Powered by <strong>Redazzalpha</strong>
+              <v-row>
+                <v-col>
+                  <v-btn
+                    v-for="link in followUsLinks"
+                    :key="link.label"
+                    :href="link.href"
+                    color="white "
+                    icon
+                    plain
+                    class="link ma-2"
+                    style="
+                      font-family: 'Raleway', sans-serif;
+                      font-weight: bold;
+                      text-decoration: unset;
+                      padding: 0;
+                    "
+                  >
+                    <v-icon size="45">
+                      {{ link.icon }}
+                    </v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+
+      <v-col>
+        <v-container fluid style="height: 100%" class="d-flex" align-center>
+          <v-row>
+            <v-col>
+              <v-row
+                justify="center"
+                no-gutters
+                style="border: solid white 1px; border-radius: 10px"
+              >
+                <v-col
+                  class="py-4 text-center white--text"
+                  cols="12"
+                  style="font-style: italic"
+                >
+                  &copy; Copyright {{ new Date().getFullYear() }} <br />
+                  All rights reserved.
+                  <br v-show="$vuetify.breakpoint.name == 'xs'" />
+                  Powered by <strong>Redazzalpha</strong>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-col>
     </v-row>
   </v-footer>
@@ -41,6 +95,12 @@ export default Vue.extend({
       { label: "LOCATION", href: "/location" },
       { label: "NOS DRÔNES", href: "/drones" },
       { label: "CONTACTS", href: "/contacts" },
+    ],
+    followUsLinks: [
+      { label: "facebook", href: "https://facebook.com", icon: "mdi-facebook" },
+      { label: "youtube", href: "https://youtube.com", icon: "mdi-youtube" },
+      { label: "instagram", href: "https://instagram.com", icon: "mdi-instagram" },
+      { label: "linkedin", href: "https://linkedin.com", icon: "mdi-linkedin" },
     ],
   }),
 });
